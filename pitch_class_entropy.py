3dba43b1abfb75467714dc7ae92ab1489c2cef47
deborahsag <deborahsag@gmail.com>
2023-03-10 11:40:43 -0300

    modified entropy script to walk through multiple directories.

--- pitch_class_entropy.py
+++ pitch_class_entropy.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pickle
 
 import numpy as np
 from scipy.stats import entropy
@@ -10,31 +11,32 @@ import pretty_midi
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-midi_dir", type=str, help="A")
+    parser.add_argument("-midi_dir", type=str, help="Folder of midi files to be evaluated.")
 
     args = parser.parse_args()
 
-    dir_path = args.midi_dir
+    directory = args.midi_dir
 
     entropies = []
 
     print("Pitch class histograms entropies:")
     print()
 
-    for midi_name in os.listdir(dir_path):
-        midi_path = os.path.join(dir_path, midi_name)
+    for root, dirs, files in os.walk(directory):
+        for midi_name in files:
+            midi_path = os.path.join(root, midi_name)
 
-        if os.path.isfile(midi_path):
-            ext = os.path.splitext(midi_path)[-1].lower()
+            if os.path.isfile(midi_path):
+                ext = os.path.splitext(midi_path)[-1].lower()
 
-            if ext == ".mid":
-                midi_data = pretty_midi.PrettyMIDI(midi_path)
-                hist = midi_data.get_pitch_class_histogram()
+                if ext == ".mid":
+                    midi_data = pretty_midi.PrettyMIDI(midi_path)
+                    hist = midi_data.get_pitch_class_histogram()
 
-                e = entropy(hist)
-                entropies.append(e)
+                    e = entropy(hist)
+                    entropies.append(e)
 
-                print(f"{midi_name} \t entropy: {e}")
+                    print(f"{midi_name} \t entropy: {e}")
 
     ent = np.array(entropies)
     ent_mean = np.mean(ent)
