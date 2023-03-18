d193b558c872c67edb326bbdd5c76570c61754c3
deborahsag <deborahsag@gmail.com>
2023-03-18 14:23:17 -0300

    corrected pitch_class_entropy.py using bootstrap method.

--- pitch_class_entropy.py
+++ pitch_class_entropy.py
@@ -1,38 +1,28 @@
 import argparse
 import os
 
-import math
 import numpy as np
-from scipy.stats import entropy, norm
+from scipy.stats import bootstrap, entropy
 
 import pretty_midi
-
-from midi_processor.processor import encode_midi
-
-
-def compute_entropy_confidence_interval(hist, num_tokens):
-    # https://math.stackexchange.com/questions/1259843/confidence-interval-of-information-entropy
-    N = num_tokens
-    var = np.sum(((np.log2(hist) + 1) ** 2) * (hist * (1 - hist) / N))
-    scale = var
-    return norm.interval(confidence=0.95, loc=0, scale=scale)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-midi_root", type=str, help="Folder of midi files to be evaluated.")
+    parser.add_argument("--print_each", action="store_true", default=False, help="Print entropy value of each piece.")
+    parser.add_argument("-n_resamples", type=int, default=9999, help="Number of resamples for the bootstrap method.")
+    parser.add_argument("-confidence_level", type=float, default=0.95, help="Confidence level for the confidence interval. Default: 0.95.")
     args = parser.parse_args()
 
-    directory = args.midi_root
-
-    original = {}
-    generated = {}
-
     print("Pitch Class Histogram Entropy")
-    print(f"MIDI directory: {directory}")
+    print(f"MIDI directory: {args.midi_root}")
     print()
 
-    for root, dirs, files in os.walk(directory):
+    # get entropy value for each piece in directory
+    original = {}
+    generated = {}
+    for root, dirs, files in os.walk(args.midi_root):
         for midi_name in files:
             midi_path = os.path.join(root, midi_name)
             ext = os.path.splitext(midi_path)[-1].lower()
@@ -41,71 +31,54 @@ def main():
                 midi_data = pretty_midi.PrettyMIDI(midi_path)
                 hist = midi_data.get_pitch_class_histogram()
                 ent = entropy(hist, base=2)
-                num_tokens = len(encode_midi(midi_path))
-                interval = compute_entropy_confidence_interval(hist, num_tokens)
 
                 if midi_name.startswith("original"):
-                    original[midi_name] = []
-                    original[midi_name].append(hist)
-                    original[midi_name].append(ent)
-                    original[midi_name].append(interval)
+                    original[midi_name] = ent
 
                 if midi_name.startswith("rand"):
-                    generated[midi_name] = []
-                    generated[midi_name].append(hist)
-                    generated[midi_name].append(ent)
-                    generated[midi_name].append(interval)
+                    generated[midi_name] = ent
 
     print("-----Original pieces-----")
+    if args.print_each:
+        for name, ent in sorted(original.items()):
+            print(f"{name}\tentropy: {ent}")
 
-    ori_hist = []
-    ori_ent = []
-    for name, info in sorted(original.items()):
-        hist = info[0]
-        ent = info[1]
-        interval = info[2]
+    # compute mean entropy
+    ori_ent = np.array(list(original.values()))
+    ori_ent_mean = np.mean(ori_ent)
+    print(f"Mean entropy:\t{ori_ent_mean}")
 
-        ori_hist.append(hist)
-        ori_ent.append(ent)
-
-        print(f"{name}:")
-        print(f"\tentropy:\t{ent}")
-        print(f"\tconfidence interval\t: {interval}")
-
-    ori_ent_mean = np.mean(np.array(ori_ent))
-    print(f"Mean entropy: {ori_ent_mean}")
-
-    # ori_hist_mean = np.mean(ori_hist)
-    # ori_interval = compute_entropy_confidence_interval(ori_hist_mean, ?)
+    # bootstrap confidence interval
+    if len(ori_ent) > 1:
+        data = (ori_ent,)
+        ori_res = bootstrap(data, np.mean, n_resamples=args.n_resamples, confidence_level=args.confidence_level, method='BCa')
+        print(f"Mean entropy confidence interval:\t{ori_res.confidence_interval}")
 
     print()
 
     print("-----Generated pieces-----")
-    gen_hist = []
-    gen_ent = []
-    for name, info in sorted(generated.items()):
-        hist = info[0]
-        ent = info[1]
-        interval = info[2]
+    if args.print_each:
+        for name, ent in sorted(generated.items()):
+            print(f"{name}\tentropy: {ent}")
 
-        gen_hist.append(hist)
-        gen_ent.append(ent)
+    # compute mean entropy
+    gen_ent = np.array(list(generated.values()))
+    gen_ent_mean = np.mean(gen_ent)
+    print(f"Mean entropy:\t{gen_ent_mean}")
 
-        print(f"{name}:")
-        print(f"\tentropy:\t{ent}")
-        print(f"\tconfidence interval\t: {interval}")
-
-    gen_ent_mean = np.mean(np.array(gen_ent))
-    print(f"Mean entropy: {gen_ent_mean}")
-
-    # gen_hist_mean = np.mean(ori_hist)
-    # gen_interval = compute_entropy_confidence_interval(ori_hist_mean, ?)
+    # bootstrap confidence interval
+    data = (gen_ent,)
+    gen_res = bootstrap(data, np.mean, n_resamples=99, confidence_level=0.95, method='BCa')
+    print(f"Mean entropy confidence interval:\t{gen_res.confidence_interval}")
 
     print()
 
     print("-----Mean entropy comparison-----")
-    print(f"Original: \t {ori_ent_mean}")
-    print(f"Generated: \t {gen_ent_mean}")
+    if len(ori_ent) > 1:
+        print(f"Original: \t {ori_ent_mean} \t {ori_res.confidence_interval}")
+    else:
+        print(f"Original: \t {ori_ent_mean}")
+    print(f"Generated: \t {gen_ent_mean} \t {gen_res.confidence_interval}")
 
 
 if __name__ == "__main__":
