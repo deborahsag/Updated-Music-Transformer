46512f970565e82f23e028a947b87509b08e744f
deborahsag <deborahsag@gmail.com>
2023-04-05 11:39:03 -0300

    added ability to get partitions of midi files with multiple instruments.

--- pitch_class_consistency.py
+++ pitch_class_consistency.py
@@ -7,31 +7,51 @@ from utilities.metrics_argument_funcs import parse_pitch_class_consistency_args,
 
 
 def get_midi_partitions(midi, num_partitions):
-    """
-    Only valid for MIDI files with a single instrument. Returns a list of Intrument object type.
-    """
-    end_time = midi.get_end_time()
+    midi_partitions = []
+    for i in range(num_partitions):
+        midi_partitions.append(pretty_midi.PrettyMIDI())
+
+    for instrument in midi.instruments:
+        instrument_partitions = get_instrument_partitions(instrument, num_partitions)
+
+        for i in range(num_partitions):
+            midi_partitions[i].instruments.append(instrument_partitions[i])
+
+    return midi_partitions
+
+
+# to be fixed: some notes are left behind.
+def get_instrument_partitions(instrument, num_partitions):
+    end_time = instrument.get_end_time()
     partition_duration = end_time // num_partitions
+    current_limit = partition_duration  # defines end of current partition in seconds
 
-    current_limit = partition_duration
-    notes = midi.instruments[0].notes
-    n = 0
+    program = instrument.program
+    notes = instrument.notes
+    n = 0   # run through entire instrument track note for note
 
-    partitions = []
+    instrument_partitions = []     # list of separate parts of the same track. Length = num_partitions
     for p in range(num_partitions):
-        part = pretty_midi.Instrument(program=1)
+        part = pretty_midi.Instrument(program=program)    # create a new object for new partition
         partition_notes = []
         while n < len(notes):
             if notes[n].start > current_limit:
                 current_limit += partition_duration
-                break
+                break   # stop populating current partition and jump to the next
             else:
                 partition_notes.append(notes[n])
                 n += 1
-        part.notes = partition_notes
-        partitions.append(part)
+        part.notes = partition_notes    # set current Instrument object with the list of Notes
+        instrument_partitions.append(part)
 
-    return partitions
+    return instrument_partitions
+
+
+def get_partitions_histograms(partitions):
+    histograms = []
+    for part in partitions:
+        histograms.append(part.get_pitch_class_histogram())
+    return histograms
 
 
 def compute_total_variation_distance(histogram_1, histogram_2):
@@ -62,13 +82,12 @@ def main():
             if ext == ".mid":
                 midi_data = pretty_midi.PrettyMIDI(midi_path)   # get midi data and divide into partitions
                 partitions = get_midi_partitions(midi_data, args.num_partitions)
+                histograms = get_partitions_histograms(partitions)  # get pitch class histograms for each partition
 
                 distances = []
-                for i in range(args.num_partitions - 1):    # calculate the distance between partitions in pairs
-                    hist_1 = partitions[i].get_pitch_class_histogram()
-                    hist_2 = partitions[i + 1].get_pitch_class_histogram()
-
-                    distances.append(compute_total_variation_distance(hist_1, hist_2))
+                for i in range(len(histograms) - 1):    # calculate the distance between histograms pair by pair
+                    dist = compute_total_variation_distance(histograms[i], histograms[i+1])
+                    distances.append(dist)
 
                 if midi_name.startswith("original"):
                     original[midi_name] = distances
--- utilities/metrics_argument_funcs.py
+++ utilities/metrics_argument_funcs.py
@@ -55,9 +55,6 @@ def parse_pitch_class_consistency_args():
 
     parser.add_argument("-midi_root", type=str, help="Folder of midi files to be evaluated.")
     parser.add_argument("-num_partitions", type=int, default=4, help="Number of partitions for pair to pair comparison.")
-    parser.add_argument("-n_resamples", type=int, default=9999, help="Number of resamples for the bootstrap method.")
-    parser.add_argument("-confidence_level", type=float, default=0.95,
-                        help="Confidence level for the confidence interval. Default: 0.95.")
 
     parser.add_argument("--print_each", action="store_true", default=False, help="Print entropy value of each piece.")
 
@@ -78,7 +75,5 @@ def print_pitch_class_consistency_args(args):
     print(SEPARATOR)
     print(f"-midi_root: {args.midi_root}")
     print(f"-num_partitions: {args.num_partitions}")
-    print(f"n_resamples: {args.n_resamples}")
-    print(f"confidence_level: {args.confidence_level}")
     print(SEPARATOR)
     print("")
